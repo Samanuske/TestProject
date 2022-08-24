ca66d15adb3fa3acb712582a152252ec30a0ef79
Samanuske <83805538+Samanuske@users.noreply.github.com>
2022-08-24 22:13:53 +0400

    Add files via upload

--- conftest.py
+++ conftest.py
@@ -12,15 +12,15 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
+    browser = webdriver.Chrome()
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome()
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    #else:
+    #    raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
